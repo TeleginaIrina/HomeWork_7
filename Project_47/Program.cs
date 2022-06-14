febcb05cbdcd92b590da2a6d7429a8a551a45c5b
Телегина Ирина <ikoska056@gmail.com>
2022-06-14 15:24:47 +0300

    project_47

---
+++ Project_47/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// 47. Показать двумерный массив размером m×n заполненный случайными вещественными числами
+
+Console.Clear();
+
+double[,] array = new double[4, 4];
+Random number = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(number.NextDouble() * 100, 2);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
