65b9fa2b3e91f59a6ddf7c0cfc41b45f64c4c719
Телегина Ирина <ikoska056@gmail.com>
2022-06-14 15:28:04 +0300

    project_50

---
+++ Project_50/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+
+Console.Clear();
+var arr = new int[5, 10];
+Random number = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = number.Next(1, 100);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool checkStatus = true;
+    int numberInt = 0;
+    while (checkStatus)
+    {
+        string? numberStr = Console.ReadLine();
+        if (int.TryParse(numberStr, out numberInt))
+        {
+            checkStatus = false;
+            break;
+        }
+        else
+            Console.WriteLine("Ввели не число. Введите число: ");
+    }
+    return numberInt;
+}
+
+void SearchNumber(int number)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            if (arr[i, j] == number)
+            {
+                Console.WriteLine($"Число {number} находится на: {i}, {j}");
+                return;
+            }
+        }
+    }
+    Console.WriteLine("В данном массиве введенное число не найдено");
+}
+
+FillArray();
+PrintArray();
+int InPutNumber = GetNumber("Введите число: ");
+SearchNumber(InPutNumber);
