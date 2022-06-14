db943b1a3789e533338c69a9dbf822891d7da1bc
Телегина Ирина <ikoska056@gmail.com>
2022-06-14 15:53:39 +0300

    project_52

--- Project_52/Program.cs
+++ Project_52/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+Console.Clear();
+var arr = new int[3, 5];
+Random randomNumber = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = randomNumber.Next(1, 100);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageSum()
+{
+    double result = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            result = result + arr[i, j];
+        }
+        Console.WriteLine($"Средняя арифметическая {j + 1} столбца = {result / arr.GetLength(0):F2}"); // :F2 - Помогает откинуть лишние цифры после запятой
+    }
+}
+
+Console.WriteLine("Даны столбцы: ");
+FillArray();
+PrintArray();
+Console.WriteLine("========================");
+AverageSum();
